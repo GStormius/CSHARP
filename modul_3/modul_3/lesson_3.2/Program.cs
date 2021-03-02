eb8fa337bf8a64d9493004cb3de325570fcfae71
N_Popov <mr.nikita.popov97@gmail.com>
2021-03-02 19:01:08 +0300

    Создан проект, инициализирована переменная

---
+++ modul_3/modul_3/lesson_3.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName; //  Объявление переменной
+            myName = "Nick"; // Инициализация переменной
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
