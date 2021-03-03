de823eae4d74a01fc01679c5add9ba0a73dd66a2
N_Popov <mr.nikita.popov97@gmail.com>
2021-03-04 00:09:21 +0300

    Добавлен проект lesson_3.4

--- modul_3/modul_3/lesson_3.3/Program.cs
+++ modul_3/modul_3/lesson_3.3/Program.cs
@@ -6,7 +6,23 @@ namespace lesson_3._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Как вас зовут? ");
+            string name = Console.ReadLine();
+
+            Console.Write("Сколько вам лет? ");
+            byte age = byte.Parse(Console.ReadLine());
+
+            Console.Write("Есть ли у вас питомец? (Да или Нет) ");
+            bool pet = Console.ReadLine() == "Да";
+
+            Console.Write("Какой у вас размер ноги? ");
+            double size = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n===Вывод===");
+            Console.WriteLine("Вас зовут {0}", name);
+            Console.WriteLine("Вам {0} лет", age);
+            Console.WriteLine("Есть ли у вас питомец? {0}", pet);
+            Console.WriteLine("У вас {0} размер ноги", size);
         }
     }
 }
---
+++ modul_3/modul_3/modul_3.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace modul_3._4
+{
+    class Program
+    {
+        enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300,
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)Semaphore.Green);
+            Console.WriteLine((Semaphore)200);
+            Console.WriteLine(Enum.GetUnderlyingType(typeof(Semaphore)));
+        }
+    }
+}
