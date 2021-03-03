a663542ee02ce3a53d6426f25a01778c9d34f68f
N_Popov <mr.nikita.popov97@gmail.com>
2021-03-03 18:56:09 +0300

    Добавлен проект lesson_3.2

--- modul_3/modul_3/lesson_3.2/Program.cs
+++ modul_3/modul_3/lesson_3.2/Program.cs
@@ -9,7 +9,18 @@ namespace lesson_3._2
             string myName; //  Объявление переменной
             myName = "Nick"; // Инициализация переменной
 
-            Console.WriteLine(myName);
+            const string Name = "Jane";
+
+            Console.WriteLine(Name);
+
+            Console.WriteLine("\tПривет, мир!");
+            Console.WriteLine("\tМне 24 года");
+            Console.WriteLine("\tMy name is {0}", myName);
+            Console.WriteLine('\u0040');  //  Символ '@'
+            Console.WriteLine('\u0023'); // Символ '#'
+
+            Console.WriteLine(0x0A);  // Число 10
+            Console.WriteLine(0b11);  // Число 3
 
             Console.ReadKey();
         }
