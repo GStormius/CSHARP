f8e3703d1fdba638f9a86d69f426be767389bd0a
N_Popov <mr.nikita.popov97@gmail.com>
2021-03-06 20:31:37 +0300

    Добавлено итоговое задание модуля

---
+++ modul_3/modul_3/lesson_3.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lesson_3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+
+            Console.Write("Enter your berthday: ");
+            var birthday =  Console.ReadLine();
+
+            Console.WriteLine("Your drthday is {0}", birthday);
+        }
+    }
+}
